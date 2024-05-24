ad036f96a57f41c428cc96bdc075d3a79ff62c30
Jared.K <3268576+jaredkotoff@users.noreply.github.com>
2024-05-23 19:16:17 -0700

    Add sorting by end_at setting

--- translate.py
+++ translate.py
@@ -166,6 +166,7 @@ class GUISettingsGeneral(TypedDict):
     tray: str
     tray_notifications: str
     priority_only: str
+    priority_by_ending_soonest: str
     proxy: str
 
 
@@ -364,7 +365,7 @@ default_translation: Translation = {
                 "tray": "Autostart into tray: ",
                 "tray_notifications": "Tray notifications: ",
                 "priority_only": "Priority Only: ",
-                "prioritze_end": "Prioritize campaigns by end date: ",
+                "priority_by_ending_soonest": "Prioritize by ending soonest: ",
                 "proxy": "Proxy (requires restart):",
             },
             "game_name": "Game name",
